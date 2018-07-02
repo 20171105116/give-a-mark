8a0a9866a5006108372a0bfa2f812416306fc446
20171105116 <1343452891@qq.com>
2018-07-02 17:46:53 +0800

    give a mark
    可以从文件1读取数据，输出到文件2

--- give a mark/main.cpp
+++ give a mark/main.cpp
@@ -1,15 +1,41 @@
-//
-//  main.cpp
-//  give a mark
-//
-//  Created by 全甲鑫 on 18/7/2.
-//  Copyright © 2018年 全甲鑫. All rights reserved.
-//
-
 #include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include<cstdlib>
+#include "algorithm"
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main(int argc, const char * argv[])
+{
+    FILE *a;
+    FILE *b;
+    b=fopen("/Users/s20171105116/Desktop/编程/give a mark/give a mark/out put.csv","a");
+    
+    char s1[200];
+    char s2[200];
+    char s3[200];
+    char s4[200];
+    char s5[200];
+    if ((a=fopen("/Users/s20171105116/Desktop/编程/give a mark/give a mark/form.csv","r"))==0)
+      {
+        printf("文件不存在\n");
+      }
+    
+       else{
+              for(int i=0;i<3;i++)
+                 {
+                   fscanf(a,"%s ,%s ,%s ,%s ,%s",s1,s2,s3,s4,s5);
+                   printf("%s %s %s %s %s\n",s1,s2,s3,s4,s5);
+                 }
+            fprintf(b,"%s ,%s ,%s ,%s ,%s \n",s1,s2,s3,s4,s5);
+           }
+    //if(i==1||i==2)
+    //{
+    //   fprintf(b,"%s ,%s ,%s ,%s ,%s ,%s ,%d ,%d ,%d ,%d ,%d \n",s1,s2,s3,s4,s5,s6,s12[1],s12[2],s12[3],s12[4],s12[5]);
+    
+    //}
+    //else{
+       // }
     return 0;
 }
