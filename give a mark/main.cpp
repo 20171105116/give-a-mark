e277f11adef9df0ee08429a0e5c2ea5541b0e992
20171105116 <1343452891@qq.com>
2018-07-02 17:34:07 +0800

    Initial Commit

---
+++ give a mark/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  give a mark
+//
+//  Created by 全甲鑫 on 18/7/2.
+//  Copyright © 2018年 全甲鑫. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
