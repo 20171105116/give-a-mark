5407b4807767e44e70afebef35901b332352c37c
20171105116 <1343452891@qq.com>
2018-07-09 14:31:32 +0800

    give a mark
    2018.7.9
    用fgetc读取文件，将字符转为浮点数，将数据单个存入，可以去掉最大最小值，但是还不能排序，和输出表格

--- give a mark/main.cpp
+++ give a mark/main.cpp
@@ -1,106 +1,154 @@
-/*#include <iostream>
-#include <vector>
-#include <string>
-#include <fstream>
-#include <sstream>
-#include<cstdlib>
-#include "algorithm"*/
-
-//int main(int argc, const char * argv[])
-//{
-    
-        /*int a[5],i,sum=0;
-        double pj;
-        int max,min;
-        for(i=0;i<5;i++)
-        {
-            printf("请%d号评委输入您所打的分数:\n",i+1);
-            scanf("%d",&a[i]);
-            sum += a[i];
-            if (i == 0){
-                //max = min = a[i];
-            }
-            else {
-                if(a[i]>max)
-                    max = a[i];
-                if(a[i]<min)
-                    min = a[i];
-            }
-        }
-        printf("该选手的得分为:");
-        for(i=0;i<5;i++)
-        {
-            printf("%d ",a[i]);
-        }
-        printf("\n该选手的最高分为%d\n",max);
-        printf("该选手的最低分为%d\n",min);
-        pj=(sum-max-min)/3.0;
-        printf("去掉一个最高分:%d,一个最低分:%d,该选手的平均分为%f\n",max,min,pj);
-        return 0 ;
-    }*/
-    /*FILE *a;
-    FILE *b;
-    //b=fopen("/Users/s20171105116/Desktop/编程/give a mark/give a mark/out put.csv","a");
-    
-    char s1[200];
-    char s2[200];
-    char s3[200];
-    char s4[200];
-    char s5[200];
-    char s6[200];
-    char s7[200];*/
-    
-    
-    /*if ((a=fopen("/Users/s20171105116/Desktop/编程/give a mark/give a mark/form.csv","r"))==0)
-      {
-       printf("文件不存在\n");
-      }
-    
-       else{
-              for(int i=0;i<20;i++)
-                 {
-                   fscanf(a," %s, %s, %s, %s, %s, %s, %s ",s1,s2,s3,s4,s5,s6,s7);
-                   printf(" %s %s %s %s %s %s %s",s1,s2,s3,s4,s5,s6,s7);
-                 }
-            fprintf(b,"%s ,%s ,%s ,%s ,%s ",s1,s2,s3,s4,s5);
-           }
-    if(i==1||i==2)
-    {
-       fprintf(b,"%s ,%s ,%s ,%s ,%s ,%s ,%d ,%d ,%d ,%d ,%d \n",s1,s2,s3,s4,s5,s6,s12[1],s12[2],s12[3],s12[4],s12[5]);
-    
-    }
-    else{
-       }
-  
-    
-    return 0;
-}*/
-
 #include<stdio.h>
+#include<stdlib.h>
+struct PF
+{
+    char numbers[20];
+    char name[50];
+    char sex[10];
+    char dateofbirth[20];
+    char cla[30];
+    char phoneNO[20];
+    char num[100][10];
+    double average;
+};
 int main()
 {
     FILE *fp;
-    char str[80];
-    fp=fopen("/Users/s20171105116/Desktop/编程/give a mark/give a mark/form.csv","r");
+    char ch;
+    struct PF s[100];
+    int i=0,l=0,m=0,n;//I行，l逗号数，m下标
+    int max=0,min=101;
+    fp=fopen("/Users/s20171105116/Desktop/studentdata.csv","r");
     if(fp==NULL)
     {
-        printf("打开文件有误");
+        printf("THIS FILE DOES NOT EXIST");
     }
-
-else
-{
-    while(fgets(str,20,fp)!=NULL)
+    else
     {
-        printf("%s",str);
+        ch=fgetc(fp);
+        while(ch!=EOF)
+        {
+            if(i==0){
+                if(ch=='\n')
+                    ++i;
+            }
+            else{
+                if(l==0){
+                    if(ch==',')
+                    {
+                        l++;
+                        m=0;
+                        ch=fgetc(fp);
+                        continue;
+                    }
+                    s[i-1].numbers[m]=ch;
+                    m++;
+                }
+                else if(l==1)
+                {
+                    if(ch==',')
+                    {
+                        l++;
+                        m=0;
+                        ch=fgetc(fp);
+                        continue;
+                    }
+                    s[i-1].name[m]=ch;
+                    m++;
+                }
+                else if(l==2)
+                {
+                    if(ch==',')
+                    {
+                        l++;
+                        m=0;
+                        ch=fgetc(fp);
+                        continue;
+                    }
+                    s[i-1].sex[m]=ch;
+                    m++;
+                }
+                else if(l==3)
+                {
+                    if(ch==',')
+                    {
+                        l++;
+                        m=0;
+                        ch=fgetc(fp);
+                        continue;
+                    }
+                    s[i-1].dateofbirth[m]=ch;
+                    m++;
+                }
+                else if(l==4)
+                {
+                    if(ch==',')
+                    {
+                        l++;
+                        m=0;
+                        ch=fgetc(fp);
+                        continue;
+                    }
+                    s[i-1].cla[m]=ch;
+                    m++;
+                }
+                else if(l==5)
+                {
+                    if(ch==',')
+                    {
+                        l++;
+                        m=0;
+                        ch=fgetc(fp);
+                        continue;
+                    }
+                    s[i-1].phoneNO[m]=ch;
+                    m++;
+                }
+                else{
+                    if(ch==',')
+                    {
+                        //只有将num定义为三维数组才可以将每个数据单个存入
+                        s[i-1].average+=atof(s[i-1].num[l-6]);//字符转浮点数
+                        if(max<atoi(s[i-1].num[l-6]))
+                            max=atoi(s[i-1].num[l-6]);
+                        if(min>atoi(s[i-1].num[l-6]))
+                            min=atoi(s[i-1].num[l-6]);
+                        l++;
+                        m=0;
+                        ch=fgetc(fp);
+                        continue;
+                    }
+                    s[i-1].num[l-6][m]=ch;
+                    m++;
+                    if(ch=='\n')
+                    {
+                        s[i-1].average+=atof(s[i-1].num[l-6]);
+                        if(max<atoi(s[i-1].num[l-6]))
+                            max=atoi(s[i-1].num[l-6]);
+                        if(min>atoi(s[i-1].num[l-6]))
+                            min=atoi(s[i-1].num[l-6]);
+                        s[i-1].average=(s[i-1].average-max-min)/(l-7);//l-7是因为去掉最大值和最小值所以以逗号的个数来控制
+                        max=0;min=101;
+                        i++;l=0;m=0;
+                    }
+                }
+            }
+            ch=fgetc(fp);
+        }
+        s[i-1].average+=atof(s[i-1].num[l-6]);
+        if(max<atoi(s[i-1].num[l-6]))
+            max=atoi(s[i-1].num[l-6]);
+        if(min>atoi(s[i-1].num[l-6]))
+            min=atoi(s[i-1].num[l-6]);
+        s[i-1].average=(s[i-1].average-max-min)/(l-7);
+        fclose(fp);
+        for(n=0;n<i;n++)
+            printf("%s,%s,%s,%s,%s,%s,%.2lf\n",s[n].numbers,s[n].name,s[n].sex,s[n].dateofbirth,s[n].cla,s[n].phoneNO,s[n].average);
+        fp=fopen("student.csv","w+");
+        fprintf(fp,"numbers,name,sex,dateofbirth,class,phoneNo,average\n");
+        for(n=0;n<i;n++)
+            fprintf(fp,"%s,%s,%s,%s,%s,%s,%.2lf\n",s[n].numbers,s[n].name,s[n].sex,s[n].dateofbirth,s[n].cla,s[n].phoneNO,s[n].average);
+        fclose(fp);
     }
-    fclose(fp);
-
-return 0;
-}
-
-
-
-
-
-
+    return 0;
 }
